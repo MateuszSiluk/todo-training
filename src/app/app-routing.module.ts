e6607f8b8154c0994ecd438fdcaba3039d9b53ae
Mateusz Siluk <65629709+MateuszSiluk@users.noreply.github.com>
2022-03-03 19:47:48 +0000

    FreshStart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+
 
 const routes: Routes = [];
 
