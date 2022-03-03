89a78542aa2791224de70426f440c61457805530
Mateusz Siluk <65629709+MateuszSiluk@users.noreply.github.com>
2022-03-03 23:02:57 +0000

    Added Const Of Photo addresses

--- src/app/pages/about-us.page.ts
+++ src/app/pages/about-us.page.ts
@@ -2,4 +2,21 @@ import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/
 
 @Component({ selector: 'lib-about-us-page', templateUrl: './about-us.page.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
 export class AboutUsPage {
+    images = {
+
+        image1: {
+        src: '../../assets/images/fake-person-1.jpg',
+        alt: 'Dani Chavez'
+        },
+
+        image2: {
+        src: '../../assets/images/fake-person-2.jpg',
+        alt: 'Katie Carter'
+        },
+
+        image3: {
+        src: '../../assets/images/fake-person-3.jpg',
+        alt: 'Kara Welsh'
+        }
+    };
 }
