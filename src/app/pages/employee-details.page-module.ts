7aa4ad8d23c32411abd067189520e4de0c7bf88b
Mateusz Siluk <65629709+MateuszSiluk@users.noreply.github.com>
2022-03-10 20:36:56 +0000

    fixed show of employee details

--- src/app/pages/employee-details.page-module.ts
+++ src/app/pages/employee-details.page-module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { EmployeeDetailsPage } from './employee-details.page';
+import { EmployeeDetailComponentModule } from '../../../projects/teams/src/lib/adapters/primary/ui/employee-detail.component-module';
 
 @NgModule({ imports: [CommonModule,
       RouterModule.forChild([
@@ -10,6 +11,7 @@ import { EmployeeDetailsPage } from './employee-details.page';
           component: EmployeeDetailsPage,
         }
       ]),
+  EmployeeDetailComponentModule
 ],
   	declarations: [EmployeeDetailsPage],
   	providers: [],
