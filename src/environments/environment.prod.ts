e2af405c930e6f55475900a980e3f28f0a0c0a7c
Mateusz Siluk <65629709+MateuszSiluk@users.noreply.github.com>
2022-03-15 22:43:39 +0000

    added setting for github/production deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyASePcEv6DngoSYfwLk3uba8QM4jOl3NxU",
+    authDomain: "lowgular-learn.firebaseapp.com",
+    projectId: "lowgular-learn",
+    storageBucket: "lowgular-learn.appspot.com",
+    messagingSenderId: "566902591069",
+    appId: "1:566902591069:web:a15d1df5b43d69742c87b4",
+    measurementId: "G-E8CZ4205CQ"
+  }
 };
